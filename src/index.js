a46bcb32a6e09ebb3bdfac217eda24a71c06a416
Jon Lazarini <jonathan.lazarini@wipro.com>
2017-02-20 23:38:58 +0000

    add basic build scripts(js, css) and server

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by jonlazarini on 20/02/17.
+ */
+var module = require('./module')
+module()
+console.log('hello world!!!')
---
+++ src/module.js
@@ -0,0 +1,6 @@
+/**
+ * Created by jonlazarini on 20/02/17.
+ */
+module.exports = function() {
+    console.log('hello module!!!!!!')
+}
